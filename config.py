fcfe98e668779b7ab86f594ce92646a1cde68df4
yulauk <yulauk@gmail.com>
2023-12-10 16:04:46 +0100

    replace specific values

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import os
 
 """in this file you need to set the API keys"""
 
-'''If you are using Docker'''
+'''If you are using Docker!'''
+# In your self-contained Docker environment, perform the following steps:
+# 1. Replace "your_actual_bot_token" with your actual Telegram bot token.
+# 2. Change "your_image_name" to your Docker image name.
+
+# After making the changes, run the following command to start the Docker container:
 # docker run -e TELEGRAM_BOT_TOKEN=your_actual_bot_token your_image_name
+
 
 '''If you are running your script outside of Docker'''
 # set api-key and token in your shell before executing the script
